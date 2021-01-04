ca10558084585ea15857b027f7f577e107718916
the <max9985053@naver.com>
2021-01-04 16:27:31 +0900

    fourth push

---
+++ cuda_check.py
@@ -0,0 +1,13 @@
+
+#This code is executed on google colab
+import torch
+
+x = torch.randn(1)
+
+if torch.cuda.is_available():
+	device = torch.device("cuda")
+	y = torch.ones_like(x, device=device)
+	x = x.to(device)
+	z = x + y
+	print(z)
+	print(z.to("cpu", torch.double))
